81ab58ec86ee799c347084d4e8c1f294f36fa83a
Brenden Niedermeyer <contact@brenden.dev>
2021-10-02 14:20:07 +0200

    feat: refactor token logic

--- cascadia-js-live-sync/main.js
+++ cascadia-js-live-sync/main.js
@@ -1,22 +1,24 @@
-﻿  const fetch = require('node-fetch');
-  const FIREBASE_URL = await bot.secrets.read('firebase-url')
+﻿const fetch = require('node-fetch');
+const FIREBASE_URL = await bot.secrets.read('firebase-url');
+const FIREBASE_TOKEN = await bot.secrets.read('firebase-token');
   
-  const body = {
-    // this `visible` field is utilized to remove messages from the stream while preserving the data
-    // e.g. if someone posts something inappropriate toggling this value in the db will remove it from view
-    visible: true,
-    text: bot.arguments,
-    username: bot.from.name,
-    email: bot.from.email,
-    created: new Date().toISOString()
-  }
+const body = {
+  // this `visible` field is utilized to remove messages from the stream while preserving the data. e.g. if someone posts something inappropriate toggling this value in the db will remove it from view
+  visible: true,
+  text: bot.arguments,
+  username: bot.from.name,
+  email: bot.from.email,
+  created: new Date().toISOString()
+};
   
-  try {
-    await fetch(FIREBASE_URL, {
-      method: 'post',
-      body: JSON.stringify(body),
-      headers: { "Content-Type": "application/json" }
-    })
+try {
+  const url = FIREBASE_TOKEN ? `${FIREBASE_URL}?access_token=${FIREBASE_TOKEN}` : FIREBASE_URL;
+
+  await fetch(url, {
+    method: 'post',
+    body: JSON.stringify(body),
+    headers: { "Content-Type": "application/json" }
+  });
   } catch(err){
     console.error('Error updating datastore: ',err)
   }
